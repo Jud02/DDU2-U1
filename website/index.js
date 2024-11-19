d2e2654696cb25d5de9a6296b7d7fc8d31fdafc6
Jud02 <judy.ynh@gmail.com>
2024-11-19 13:27:57 +0100

    Start

---
+++ website/index.js
@@ -0,0 +1,9 @@
+// Recommended: All functions declared here
+
+// Recommended: constants with references to existing HTML-elements
+
+// Recommended: Ask for the city name and then the rest of the code
+
+
+let cityName = prompt("Vilken stad?")
+

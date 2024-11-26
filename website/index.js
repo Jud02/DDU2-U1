7a9cbab59abdaa782e9768d0a03d5c2b0ef586a9
Jud02 <judy.ynh@gmail.com>
2024-11-26 08:33:47 +0100

    Loop för att gå igenom städerna och skriva ut dom på h2 osv
    Beskrivning:
    
    Lade till en loop som söker igenom en lista av städer och jämför med användarens inmatning.
    
    Uppdaterade <h2>- och <title>-elementen på sidan med stadens namn och land om en matchning hittas.
    
    Markerade den matchade stadens .cityBox med en CSS-klass (target) för att indikera val.
    
    Lade till en cityFound-variabel för att spåra om en matchning hittats och avsluta loopen vid träff.

--- website/index.js
+++ website/index.js
@@ -7,13 +7,24 @@
 
 let cityName = prompt("Vilken stad?")
 let cityFinder = prompt("Vilken stad?");
+let cityFound = false;
 
 function miniBox(cityName) {
     const cityDiv = document.createElement("div");
     cityDiv.classList.add("cityBox");
     cityDiv.textContent = cityName; 
+for (let i = 0; i < cities.length; i++) {
+  if (cityFinder == cities[i].name) {
+      document.querySelector("h2").textContent = `${cities[i].name} (${cities[i].country})`;
+      document.querySelector("title").textContent = `${cities[i].name}`;
+
+      const cityDivs = document.querySelectorAll(".cityBox"); 
+      cityDivs[i].classList.add("target");
 
     document.getElementById("cities").append(cityDiv);
+      cityFound = true;
+      break; 
+  }
 }
 for (let i = 0; i < cities.length; i++) {
     miniBox(cities[i].name)
